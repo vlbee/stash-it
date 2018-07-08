4e930bf3586aec4c74b15e2e4a029df79c356763
vlbee <32312712+vlbee@users.noreply.github.com>
2018-07-08 22:54:06 +0100

    add getLocation function to App

--- src/components/app.js
+++ src/components/app.js
@@ -7,23 +7,36 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      lat: 43.505,
-      long: -75.09,
+      lat: '',
+      long: '',
       by_distance: false,
       open_late: false,
-      twentyfour_seven: false
+      twentyfour_seven: false,
+      stashpoints: ''
     };
   }
 
-  // getLocation = () = {
 
-  // }
+  getLocation = () => {
+    const success = position => {
+      console.log(position)
+      this.setState({
+        lat: position.coords.latitude,
+        long: position.coords.longitude
+      })
+    }
+    const unsuccessful = error => console.error(`ERROR(${error.code}): ${error.message}`)
+    navigator.geolocation.getCurrentPosition(success, unsuccessful, { timeout: 5000 })
+  }
 
-  // componentDidMount() {
-  //   fetchRequest().then(res => {
-  //     console.log(res[0])
-  //   })
-  // }
+  componentDidMount() {
+    this.getLocation()
+    //   // fetchRequest().then(res => {
+    //   //   this.setState({
+    //   //     stashpoints: res
+    //   //   })
+    //   // })
+  }
 
 
   render() {
