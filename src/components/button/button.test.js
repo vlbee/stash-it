73975c9668165713575c6633595cc3a53eac3b94
vlbee <32312712+vlbee@users.noreply.github.com>
2018-07-08 11:37:18 +0100

    setup dummy data and jest-fetch-mock

--- src/components/button/button.test.js
+++ src/components/button/button.test.js
@@ -6,7 +6,7 @@ import Button from './button';
 test('Button works', () => {
 
   const { container, getByText } = render(<Button>Test</Button>);
-  console.log(prettyDOM(container))
+  // console.log(prettyDOM(container))
   const buttonNode = getByText('Test');
   fireEvent.click(buttonNode);
   expect(buttonNode.textContent).toBe('Toggled');
