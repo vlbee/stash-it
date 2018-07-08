e8f51c5aa2ff19e7cb185ae774dc1133d80d69b0
vlbee <32312712+vlbee@users.noreply.github.com>
2018-07-08 17:49:54 +0100

    add state

--- src/components/app.js
+++ src/components/app.js
@@ -1,30 +1,35 @@
-// import React from 'react';
-
-
-
-// export default class App extends React.Component {
-//   render() {
-//     return (<div className="app">
-//       <h1 className="app__title">Hello World!</h1>
-
-//     </div>)
-//   }
-// }
-
 import React, { Component } from 'react';
-import Mapview from './map/mapview'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+
 const position = [51.505, -0.09]
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: '',
+      long: '',
+      by_distance: false,
+      open_late: false,
+      twentyfour_seven: false
+    };
+  }
+
+  // getLocation = () = {
+
+  // }
+
+  componentDidMount() {
+
+  }
+
   render() {
-
     return (
-
       <div className="app">
         <h1>Find It, Stash It</h1>
-        {/* <Mapview /> */}
-        <Map ref="map" center={[51.505, -0.09]} zoom={13} className="map">
+
+        <Map ref="map" center={[51.505, -0.09]} zoom={3} className="map">
           <TileLayer
             url="https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}@2x.png?access_token=pk.eyJ1IjoidmxiZWUiLCJhIjoiY2pqY3psbW91M3ZydzN2cWdsM202OWVuMSJ9.POAfk9YduZ5FIIMMjKC4pQ"
             attribution='&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
@@ -34,9 +39,7 @@ class App extends Component {
           </Marker>
         </Map>
       </div>
-
     )
-
   }
 
 }
