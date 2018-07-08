9bbc5d97fca7009bff91ad9384ad0e39b1af8d3f
vlbee <32312712+vlbee@users.noreply.github.com>
2018-07-08 19:14:43 +0100

    add api token config

--- src/components/mapview.js
+++ src/components/mapview.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import ErrorBoundary from './errorBoundary'
+import { token } from './token'
 
-
+const mapboxURL = `https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}@2x.png?access_token=${token}`
 const position = [51.505, -0.09]
 
 class Mapview extends Component {
@@ -16,7 +17,7 @@ class Mapview extends Component {
       <div>
         <Map ref="map" center={[51.505, -0.09]} zoom={3} className="map">
           <TileLayer
-            url="https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}@2x.png?access_token=pk.eyJ1IjoidmxiZWUiLCJhIjoiY2pqY3psbW91M3ZydzN2cWdsM202OWVuMSJ9.POAfk9YduZ5FIIMMjKC4pQ"
+            url={mapboxURL}
             attribution='&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
           />
           <Marker position={position}>
