fa69b546021e2c68a2c3cb3baa67e3babbb3f0d8
vlbee <32312712+vlbee@users.noreply.github.com>
2018-07-09 13:11:12 +0100

    add stashpoint fetching to app and update state

--- src/components/app.js
+++ src/components/app.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
 import Mapview from './mapview'
 // import { fetchRequest } from '../utils/fetch/fetch'
-// import { getLocation } from '../utils/getLocation/getLocation'
+
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      lat: null,
-      long: null,
-      nearby_radius: 5,
-      by_distance: "asc",
-      open_late: false,
-      twentyfour_seven: false,
-      stashpoints: [],
+      query: {
+        centre_lat: null,
+        centre_lon: null,
+        nearby_radius: 5,
+        by_distance: "desc",
+        open_late: false,
+        twentyfour_seven: false
+      },
+      stashpoints: null
       //TODO: add error message feedback to user if geolocation or data fetching fails
-      errorMsg: null
+      // errorMsg: null
     };
   }
 
@@ -32,18 +34,55 @@ class App extends Component {
     }
   }
 
+  checkFetchResponse = res => {
+    if (res.status === 200) {
+      let result = res.json();
+      return result;
+    } else {
+      console.error(
+        `Error with fetch request. Response: ${res.status}`
+      );
+    }
+  };
+
+  fetchRequest = (params = {}) => {
+    const url = new URL(`https://api-staging.stasher.com/v1/stashpoints`);
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+    console.log(url.search)
+    return fetch(url.toString(), {
+      method: 'get',
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+      .then(this.checkFetchResponse)
+      .catch(err => {
+        throw new Error(`Fetch request failed: ${err}`);
+      });
+  };
+
   componentDidMount() {
     this.getLocation().then(position => {
       this.getLocation = null;
       if (position.coords) {
-        this.setState({
-          lat: position.coords.latitude,
-          long: position.coords.longitude
+        this.setState(prevState => {
+          return {
+            query: {
+              centre_lat: position.coords.latitude,
+              centre_lon: position.coords.longitude,
+              nearby_radius: prevState.query.nearby_radius,
+              by_distance: prevState.query.by_distance,
+              open_late: prevState.query.open_late,
+              twentyfour_seven: prevState.query.twentyfour_seven
+            },
+            stashpoints: prevState.stashpoints
+          }
         })
-        console.log(this.state)
       } else {
         alert('Geolocation is not supported by this browser.');
       }
+    }).then(() => {
+      this.fetchRequest(this.state.query).then(data => { this.setState({ stashpoints: data }); console.log(this.state) })
     }).catch(error => {
       console.log(error)
     })
@@ -55,14 +94,12 @@ class App extends Component {
     }
   }
 
-
-
   render() {
     return (
       <div className="app" >
         <h1>Find It, Stash It</h1>
-        {(this.state.lat === null) ? (<h2>Fetching Location data</h2>) :
-          (<Mapview stashpoints={this.state.stashpoints} lat={this.state.lat} long={this.state.long}></Mapview>)}
+        {(this.state.query.centre_lat === null) ? (<h2>Fetching Location Data</h2>) :
+          (<Mapview stashpoints={this.state.stashpoints} lat={this.state.query.centre_lat} long={this.state.query.centre_lon}></Mapview>)}
       </div>
     )
   }
