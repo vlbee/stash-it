da7478d827c0e69a8b86a438912c95bc4f053555
vlbee <32312712+vlbee@users.noreply.github.com>
2018-07-09 15:48:03 +0100

    move attribution text to varible and change map marker key to location.id

--- src/components/mapview.js
+++ src/components/mapview.js
@@ -6,6 +6,7 @@ import { token } from '../token'
 
 // Source of map tiles
 const mapboxURL = `https://api.tiles.mapbox.com/v4/mapbox.light/{z}/{x}/{y}@2x.png?access_token=${token}`
+const mapboxAttribution = '&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
 
 // Fix for react-leaflet 404 error rendering Leaflet's default market icon
 import icon from 'leaflet/dist/images/marker-icon.png';
@@ -32,11 +33,11 @@ class Mapview extends Component {
         <Map ref="map" center={[this.props.lat, this.props.long]} zoom={13} className="map">
           <TileLayer
             url={mapboxURL}
-            attribution='&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>'
+            attribution={mapboxAttribution}
           />
-          {stashpoints.map((location, i) => {
+          {stashpoints.map((location) => {
             console.log(location)
-            return (<Marker key={i} position={[location.latitude, location.longitude]}>
+            return (<Marker key={location.id} position={[location.latitude, location.longitude]}>
               <Popup>{location.name}<br />{location.address}</Popup>
             </Marker>)
           })}
