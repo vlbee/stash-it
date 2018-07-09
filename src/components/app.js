cce733d29218eb4ef0fd57c4bb3c89cc99612c5c
vlbee <32312712+vlbee@users.noreply.github.com>
2018-07-09 11:41:51 +0100

    make geolocation fetch async

--- src/components/app.js
+++ src/components/app.js
@@ -1,49 +1,68 @@
 import React, { Component } from 'react';
 import Mapview from './mapview'
-import fetchRequest from '../utils/fetch/fetch'
+// import { fetchRequest } from '../utils/fetch/fetch'
+// import { getLocation } from '../utils/getLocation/getLocation'
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      lat: '',
-      long: '',
-      by_distance: false,
+      lat: null,
+      long: null,
+      nearby_radius: 5,
+      by_distance: "asc",
       open_late: false,
       twentyfour_seven: false,
-      stashpoints: ''
+      stashpoints: [],
+      //TODO: add error message feedback to user if geolocation or data fetching fails
+      errorMsg: null
     };
   }
 
-
   getLocation = () => {
-    const success = position => {
-      console.log(position)
-      this.setState({
-        lat: position.coords.latitude,
-        long: position.coords.longitude
-      })
+    if (navigator.geolocation) {
+      return new Promise(
+        (resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject)
+      )
+    } else {
+      return new Promise(
+        resolve => resolve({})
+      )
     }
-    const unsuccessful = error => console.error(`ERROR(${error.code}): ${error.message}`)
-    navigator.geolocation.getCurrentPosition(success, unsuccessful, { timeout: 5000 })
   }
 
   componentDidMount() {
-    this.getLocation()
-    //   // fetchRequest().then(res => {
-    //   //   this.setState({
-    //   //     stashpoints: res
-    //   //   })
-    //   // })
+    this.getLocation().then(position => {
+      this.getLocation = null;
+      if (position.coords) {
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude
+        })
+        console.log(this.state)
+      } else {
+        alert('Geolocation is not supported by this browser.');
+      }
+    }).catch(error => {
+      console.log(error)
+    })
   }
+
+  componentWillUnmount() {
+    if (this.getLocation) {
+      this.getLocation.cancel();
+    }
+  }
+
 
 
   render() {
     return (
-      <div className="app">
+      <div className="app" >
         <h1>Find It, Stash It</h1>
-        <Mapview lat={this.state.lat} long={this.state.long}></Mapview>
+        {(this.state.lat === null) ? (<h2>Fetching Location data</h2>) :
+          (<Mapview stashpoints={this.state.stashpoints} lat={this.state.lat} long={this.state.long}></Mapview>)}
       </div>
     )
   }
